9623213d631ab39e674a0138fab75e1b9e07b848
Jhonatan Neves <nevesjn2@gmail.com>
2022-11-24 22:58:53 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appcaixa/login/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.appcaixa.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appcaixa.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
